0c5d358464a75d810541b2d81b225403e8e07925
William Snider <williamgraysonsnider@gmail.com>
2023-07-24 12:38:02 -0400

    Handle hanging of tone

--- tone/tone.py
+++ tone/tone.py
@@ -1,9 +1,9 @@
 import nidaqmx
 import numpy as np
-import matplotlib.pyplot as plt
+import time
 
 # Inputs
-FREQUENCY = 500  # FREQUENCY of the tone in Hz
+FREQUENCY = 400  # FREQUENCY of the tone in Hz
 DURATION = 0.25  # DURATION of the tone in seconds
 SAMPLE_RATE = 44100  # Sample rate in Hz (you can adjust this if needed)
 
@@ -26,12 +26,5 @@ with nidaqmx.Task() as task:
     task.write(tone_scaled, auto_start=True)
 
     # Stop the task
-    task.wait_until_done(timeout=DURATION + 1)
+    time.sleep(DURATION)  # Handles tone hanging
     task.stop()
-
-# Plot the tone
-ax = plt.subplot(111)
-ax.plot(t, tone)
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("Voltage (V)")
-plt.show()
